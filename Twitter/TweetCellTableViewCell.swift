4d1b686b20399cbe40b8defbdc20f80dfb1546f4
Tanaka Muchemwa <muchemwatanaka99@gmail.com>
2021-07-24 17:40:18 -0400

    update

---
+++ Twitter/TweetCellTableViewCell.swift
@@ -0,0 +1,33 @@
+//
+//  TweetCellTableViewCell.swift
+//  Twitter
+//
+//  Created by Tanaka Muchemwa on 7/24/21.
+//  Copyright © 2021 Dan. All rights reserved.
+//
+
+import UIKit
+
+class TweetCellTableViewCell: UITableViewCell {
+
+    
+    @IBOutlet weak var profileImage: UIImageView!
+    
+    
+    @IBOutlet weak var userNameLabel: UILabel!
+    
+    
+    @IBOutlet weak var tweetContent: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
